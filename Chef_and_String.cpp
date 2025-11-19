9cc8b111931b1b21b9d76beee70e0aaeb4b09e34
jui1960 <juifull196044@gmail.com>
2025-11-19 21:58:38 +0600

    new

---
+++ Chef_and_String.cpp
@@ -0,0 +1,54 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k;
+        cin>>n>>k;
+        string s;
+        cin >> s;
+
+        vector<char> f;
+        f.reserve(n);
+        for (char c : s)
+        {
+            if (c != 'I')
+                f.push_back(c);
+        }
+
+        long long m = f.size();
+
+        if (m == 0)
+        {
+            cout << (n * k - 1) << '\n';
+            continue;
+        }
+
+        long long dinternal = 0;
+        for (int i = 0; i + 1 < m; i++)
+        {
+            if (f[i] != f[i + 1])
+                dinternal++;
+        }
+
+        long long crs = (f.back() != f.front()) ? 1 : 0;
+
+        long long tr = k * dinternal + (k - 1) * crs;
+
+        long long total = n * k;
+        long long ans = total - 1 - tr;
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
