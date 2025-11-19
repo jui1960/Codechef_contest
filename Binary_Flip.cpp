f02e6db350b55947e4d1fc089b08b72746720df1
jui1960 <juifull196044@gmail.com>
2025-11-19 20:28:19 +0600

    add

--- Binary_Flip.cpp
+++ Binary_Flip.cpp
@@ -1,3 +1,39 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n;
+//         string s;
+//         cin >> n >> s;
+
+//         int cnt = 0, cntt = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (s[i] == '0' && s[i + 1] == '0')
+//                 cnt++;
+//             if (s[i] == '1' && s[i + 1] == '1')
+//                 cntt++;
+//         }
+
+//         if (cntt >= cnt)
+//             cout << 0 << '\n';
+
+//         else
+//             cout << ((cnt - cntt + 1) / 2) << '\n';
+//     }
+
+//     return 0;
+// }
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- Exit_Door.cpp
+++ Exit_Door.cpp
@@ -1,50 +1,51 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long int
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<ll> v(n);
-        vector<ll> p(n + 1);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> v[i];
-            p[v[i]] = i;
-        }
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+//         vector<ll> v(n);
+//         vector<ll> p(n + 1);
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> v[i];
+//             p[v[i]] = i;
+//         }
 
-        vector<bool> left(n, true);
-        ll a = 0;
-        for (int j = n; j >= 1; j--)
-        {
-            int i = p[j];
+//         vector<bool> left(n, true);
+//         ll a = 0;
+//         for (int j = n; j >= 1; j--)
+//         {
+//             int i = p[j];
 
-            int l = 0, r = 0;
-            for (int k = 0; k < i; k++)
+//             int l = 0, r = 0;
+//             for (int k = 0; k < i; k++)
 
-            {
-                if (left[k])
-                    l++;
-            }
+//             {
+//                 if (left[k])
+//                     l++;
+//             }
 
-            for (int k = i + 1; k < n; k++)
+//             for (int k = i + 1; k < n; k++)
 
-            {
-                if (left[k])
-                    r++;
-            }
-            a += min(l, r);
-            left[i] = false;
-        }
-        cout << a << '\n';
-    }
+//             {
+//                 if (left[k])
+//                     r++;
+//             }
+//             a += min(l, r);
+//             left[i] = false;
+//         }
+//         cout << a << '\n';
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
