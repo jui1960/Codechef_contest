1f501c096d1f7f33def356d9a4762212f53cd370
jui1960 <juifull196044@gmail.com>
2025-12-17 21:28:04 +0600

    make_an_app

---
+++ Make_An_AP.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<ll> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        ll diff = 0, ans = 0;
+        for (int i = 0; i + 1 < n; i++)
+            diff = __gcd(diff, v[i + 1] - v[i]);
+        for (int i = 0; i + 1 < n; i++)
+            ans += (v[i + 1] - v[i]) / diff - 1;
+        cout << ans << '\n';
+    }
+    return 0;
+}
