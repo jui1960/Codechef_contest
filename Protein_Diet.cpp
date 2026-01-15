e57110fafcabbf459c1b030d556f6e00e8c54a6a
jui1960 <juifull196044@gmail.com>
2026-01-15 18:22:20 +0600

    point_diet

---
+++ Protein_Diet.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a,b;
+    cin>>a>>b;
+    if(a>=b) cout<<"YES\n";
+    else cout<<"NO\n";
+
+    return 0;
+}
