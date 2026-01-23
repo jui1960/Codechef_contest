94d3e62dd92db96e2fda7d6711167019453130fe
jui1960 <juifull196044@gmail.com>
2026-01-23 17:06:43 +0600

    chef_and_s0ckes

---
+++ Chef_and_Socks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+      int a,b,c;
+      cin>>a>>b>>c;
+      if(a<=b+c)cout<<"YES\n";
+      else cout<<"NO\n";
+
+    
+
+    return 0;
+}
