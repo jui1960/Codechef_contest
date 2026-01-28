76b2e9b37606d748930757a2b45e32e8541028c5
jui1960 <juifull196044@gmail.com>
2026-01-28 20:35:32 +0600

    cashback

---
+++ Cashback.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    if(n>=200) cout<<n-50<<'\n';
+    else cout<<n<<'\n';
+
+    return 0;
+}
