1582beacf3a65391f2fdbbee1db07fdab1c6586a
jui1960 <juifull196044@gmail.com>
2026-02-11 20:37:39 +0600

    update

--- Payment_Scheme.cpp
+++ Payment_Scheme.cpp
@@ -1,16 +1,30 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin>>n;
+//     if(100+4*n>300){
+//         cout<<300<<'\n';
+//     }
+//     else cout<<100+4*n<<'\n';
+
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int n;
-    cin>>n;
-    if(100+4*n>300){
-        cout<<300<<'\n';
-    }
-    else cout<<100+4*n<<'\n';
-
+    int x;
+    cin>>x;
+    cout<<min((100+(x*4)),300)<<'\n';
     return 0;
 }
